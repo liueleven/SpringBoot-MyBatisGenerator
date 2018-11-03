bc6b588dd8b140a82726c580deddb869f16e0093
liueleven <liueleven@aliyun.com>
2018-11-03 11:16:02 +0800

    first commit

---
+++ src/main/java/com/eleven/springbootmbg/SpringbootmbgApplication.java
@@ -0,0 +1,13 @@
+package com.eleven.springbootmbg;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmbgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmbgApplication.class, args);
+	}
+}
---
+++ src/main/java/com/eleven/springbootmbg/entity/user.java
@@ -0,0 +1,48 @@
+package com.eleven.springbootmbg.entity;
+
+public class user {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String phone;
+
+    public user(Integer id, String username, String password, String phone) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public user() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+
+}
